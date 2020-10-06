792955237d97c689f56692d361e81f5ac033ebd6
Leah Riffell <leah.riffell@gmail.com>
2020-10-06 17:55:01 -0600

    Validate presence of party date and time, attendee_type

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,6 +1,6 @@
 class Party < ApplicationRecord
   belongs_to :movie
 
-  validates :party_date
-  validates :start_time
+  validates :party_date, presence: true
+  validates :start_time, presence: true
 end
--- app/models/party_user.rb
+++ app/models/party_user.rb
@@ -2,7 +2,7 @@ class PartyUser < ApplicationRecord
   belongs_to :user
   belongs_to :party
 
-  validates :attendee_type
+  validates :attendee_type, presence: true
 
   enum role: { 'host': 0, 'guest': 1 }
 end
