015a8e05e4c204f82aa53b19c11cf0abf5f6bb64
Leah Riffell <leah.riffell@gmail.com>
2020-10-06 21:23:25 -0600

    Address rubocop violations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,19 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       flash[:success] = "Welcome, #{user.email}!"
       redirect_to(dashboard_path)
-    elsif !user 
-      flash[:error] = "No user exists with that email address."
-      redirect_to(root_path)
-    else 
-      flash[:error] = "Incorrect password"
-      redirect_to(root_path)
+    else
+      invalid_credentials_redirect
     end
+  end
+
+  private
+
+  def invalid_credentials_redirect
+    if !User.find_by(email: params[:email])
+      flash[:error] = 'No user exists with that email address.'
+    else
+      flash[:error] = 'Incorrect password'
+    end
+    redirect_to(root_path)
   end
 end
