b5016b3a5930a231da6843953c906237e87a947a
Leah Riffell <leah.riffell@gmail.com>
2020-10-06 20:47:30 -0600

    Add ability to log into app with correct credentials
    Co-authored-by: Dan Halverson <63027888+dhalverson@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/register', to: 'users#new', as: :registration
+
+  post "/login", to: "sessions#create"
+
+  get '/dashboard', to: 'dashboard#index'
 end
