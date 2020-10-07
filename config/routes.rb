04acd85e4eac23f0b1f33519557beafa39561cf8
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-10-07 14:37:21 -0600

    PR Comment Updates Updates testing to account for sad path of registering Updates registration form to better variable syntax Updates routing for dashboard and registration to use path
    Co-authored-by: Leah Riffell <34531014+leahriffell@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/register', to: 'users#new', as: :registration
+  post '/users', to: 'users#create'
 
   post "/login", to: "sessions#create"
 
   get '/dashboard', to: 'dashboard#index'
-  
-  post '/users', to: 'users#create'
+
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'When a user visits theregistration_pathpath' do
+RSpec.describe 'When a user visits the registration path' do
   before(:each) do
-    @user1 = FactoryBot.create!(:user)
+    @user1 = FactoryBot.create(:user)
   end
 
   it 'I see a form to register including email, password, password confirmation, and a button to submit' do
     visit(registration_path)
 
-    expect(page).to have_field(:email)
-    expect(page).to have_field(:password)
-    expect(page).to have_field(:password_confirmation)
+    expect(page).to have_field('user[email]')
+    expect(page).to have_field('user[password]')
+    expect(page).to have_field('user[password_confirmation]')
 
     expect(page).to have_button('Create User')
   end
@@ -18,39 +18,38 @@ RSpec.describe 'When a user visits theregistration_pathpath' do
   it 'I see an error message and remain on the same page if I dont fill out all required fields' do
     visit(registration_path)
 
-    fill_in :email, with: ''
-    fill_in :password, with: "#{@user1.password}"
-    fill_in :password_confirmation, with: "#{@user1.password}"
+    fill_in 'user[email]', with: ''
+    fill_in 'user[password]', with: "#{@user1.password}"
+    fill_in 'user[password_confirmation]', with: "#{@user1.password}"
 
     click_button('Create User')
-    expect(current_path).to eq(registration_path)
-    expect(page).to have_content('Email cannot be blank')
+    expect(page).to have_content("Email can't be blank")
   end
 
   it 'I see an error message and remain on the same page if my email is not unique' do
     visit(registration_path)
-    fill_in :email, with: "#{@user1.email}"
-    fill_in :password, with: "#{@user1.password}"
-    fill_in :password_confirmation, with: "#{@user1.password}"
+    fill_in 'user[email]', with: "#{@user1.email}"
+    fill_in 'user[password]', with: "#{@user1.password}"
+    fill_in 'user[password_confirmation]', with: "#{@user1.password}"
     click_button('Create User')
 
     visit(registration_path)
-    fill_in :email, with: "#{@user1.email}"
-    fill_in :password, with: "#{@user1.password}"
-    fill_in :password_confirmation, with: "#{@user1.password}"
+    fill_in 'user[email]', with: "#{@user1.email}"
+    fill_in 'user[password]', with: "#{@user1.password}"
+    fill_in 'user[password_confirmation]', with: "#{@user1.password}"
     click_button('Create User')
-    expect(current_path).to eq(registration_path)
-    expect(page).to have_content('Error, email address already in use')
+    expect(page).to have_content('Email has already been taken')
   end
 
   it 'I fill out all fields, and use an unique email, click create user, I am logged in and returned to the dashboard page' do
+
     visit(registration_path)
-    fill_in :email, with: "#{@user1.email}"
-    fill_in :password, with: "#{@user1.password}"
-    fill_in :password_confirmation, with: "#{@user1.password}"
+    fill_in 'user[email]', with: 'elmer@barkmail.com'
+    fill_in 'user[password]', with: '1234'
+    fill_in 'user[password_confirmation]', with:'1234'
     click_button('Create User')
 
     expect(current_path).to eq('/dashboard')
-    expect(page).to have_content("Welcome #{@user1.email}!")
+    expect(page).to have_content('Welcome elmer@barkmail.com!')
   end
 end
