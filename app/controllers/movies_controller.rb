ee0b537183bbee4cab85c547e0060d3dea5c9e2d
Leah Riffell <leah.riffell@gmail.com>
2020-10-07 17:49:37 -0600

    Add Discover Page and feature tests Discover page has: - button to find top 40 movies - field to search by title keyword
    Co-authored-by: Dan Halverson <63027888+dhalverson@users.noreply.github.com>

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,6 @@
+class MoviesController < ApplicationController
+  def index; end
+
+  # same route but different views | same route but different content 
+  # pass needed API params with button or search bar. then if params include top 40, render top view. else, return search result view. 
+end
