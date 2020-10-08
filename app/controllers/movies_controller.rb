e554b90adf92f838d44e180c2724a2dcde80c9eb
Leah Riffell <leah.riffell@gmail.com>
2020-10-07 19:56:24 -0600

    Create helper methods for separate endpoints

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,24 +8,22 @@ class MoviesController < ApplicationController
   end
 
   def top_40
-    response_1 = conn.get("3/discover/movie?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1")
+    response_1 = top_40_endpoint("1")
+    response_2 = top_40_endpoint("2")
     
-    response_2 = conn.get("3/discover/movie?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=2")
-    
-    json_1 = JSON.parse(response_1.body, symbolize_names: true)
-    json_2 = JSON.parse(response_2.body, symbolize_names: true)
+    json_1 = parse(response_1)
+    json_2 = parse(response_2)
 
     @movies_1 = json_1[:results]
     @movies_2 = json_2[:results]
   end
   
   def keyword_search
-    response_1 = conn.get("3/search/movie?api_key=#{ENV['MOVIES_API_KEY']}&query=#{params[:keyword_search]}&page=1")
+    response_1 = keyword_search_endpoint("1")
+    response_2 = keyword_search_endpoint("2")
     
-    response_2 = conn.get("3/search/movie?api_key=#{ENV['MOVIES_API_KEY']}&query=#{params[:keyword_search]}&page=2")
-    
-    json_1 = JSON.parse(response_1.body, symbolize_names: true)
-    json_2 = JSON.parse(response_2.body, symbolize_names: true)
+    json_1 = parse(response_1)
+    json_2 = parse(response_2)
 
     @movies_1 = json_1[:results]
     @movies_2 = json_2[:results]
@@ -34,8 +32,18 @@ class MoviesController < ApplicationController
   private 
 
   def conn
-    Faraday.new(url: 'https://api.themoviedb.org') do |faraday|
-      faraday.headers['X-API-Key'] = ENV['MOVIES_API_KEY']
-    end
+    Faraday.new(url: 'https://api.themoviedb.org')
+  end
+
+  def top_40_endpoint(page_num)
+    conn.get("3/discover/movie?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=#{page_num}")
+  end
+
+  def keyword_search_endpoint(page_num)
+    conn.get("3/discover/movie?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=#{page_num}")
+  end
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
