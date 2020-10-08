a913b65d0815f9bbe49668b95a4a23d52daf385f
Leah Riffell <leah.riffell@gmail.com>
2020-10-07 20:01:23 -0600

    Combine api call actions into 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,27 +1,20 @@
 class MoviesController < ApplicationController
   def index
-    if params[:top_40]
-      top_40
+    if params[:top]
+      fetch_movies('top')
     else 
-      keyword_search
+      fetch_movies('search')
     end
   end
 
-  def top_40
-    response_1 = top_40_endpoint("1")
-    response_2 = top_40_endpoint("2")
-    
-    json_1 = parse(response_1)
-    json_2 = parse(response_2)
-
-    @movies_1 = json_1[:results]
-    @movies_2 = json_2[:results]
-  end
-  
-  def keyword_search
-    response_1 = keyword_search_endpoint("1")
-    response_2 = keyword_search_endpoint("2")
-    
+  def fetch_movies(request_type)
+    if request_type == 'top'
+      response_1 = top_40_endpoint("1")
+      response_2 = top_40_endpoint("2")
+    else 
+      response_1 = top_40_endpoint("1")
+      response_2 = top_40_endpoint("2")
+    end
     json_1 = parse(response_1)
     json_2 = parse(response_2)
 
@@ -40,7 +33,7 @@ class MoviesController < ApplicationController
   end
 
   def keyword_search_endpoint(page_num)
-    conn.get("3/discover/movie?api_key=#{ENV['MOVIES_API_KEY']}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=#{page_num}")
+    conn.get("3/search/movie?api_key=#{ENV['MOVIES_API_KEY']}&query=#{params[:keyword_search]}&page=#{page_num}")
   end
 
   def parse(response)
