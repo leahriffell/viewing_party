138634c7348447cb3cfbf3b06fd0f68091fe98bb
Leah Riffell <leah.riffell@gmail.com>
2020-10-08 21:38:49 -0600

    Create tests accessing movie show page from movie index

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe 'Movies page' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
-    it "when I type movies_path in browser, I am defaulted to being shown top movies" do
+    it 'when I type movies_path in browser, I am defaulted to being shown top movies' do
+      # I don't think this test is really testing anything unless we assert that it includes a certain title / ranking / etc.
       VCR.use_cassette('top_movies') do
-
         visit movies_path
 
         expect(current_path).to eq(movies_path)
@@ -17,42 +17,73 @@ RSpec.describe 'Movies page' do
       end
     end
 
-    describe 'I am shown search results even when' do 
-      it 'search terms include 2 words' do
-        VCR.use_cassette('two_keyword_search') do
-          visit discover_path
-
-          fill_in :keyword_search, with: 'toy story'
-          click_button('Find Movies')
-
+    describe 'when I searched for top movies' do 
+      it 'I can see information about each movie and link to show page' do
+        VCR.use_cassette('top_movies') do
+          visit movies_path
+  
           expect(current_path).to eq(movies_path)
-          expect(page).to have_content('Search Results')
-        end
-      end
+          expect(page).to have_content('Top Movies')
 
-      it 'search terms return 20 or less movie results' do
-        VCR.use_cassette('one_page_results') do
-          visit discover_path
-
-          fill_in :keyword_search, with: 'star wars'
-          click_button('Find Movies')
-
-          expect(current_path).to eq(movies_path)
-          expect(page).to have_content('Search Results')
+          within(first('.movie')) do
+            expect(page).to have_css('.title')
+            expect(page).to have_css('.vote_avg')
+            VCR.use_cassette('show_movie_details') do
+              #NOTE: I think this error may be because 'Casino' does not appear on top movies page? 
+              page.find('.title').click
+              expect(page).to have_button('Create Viewing Party for Movie')
+            end
+          end
         end
       end
     end
 
-  #   it 'when I click button to find top movies, I am redirected to movies page with top movies' do
-  #     VCR.use_cassette('top_movies') do
-  #       visit discover_path
+    describe 'when I searched by keyword' do
+      it 'I can see information about each movie and link to show page' do 
+        VCR.use_cassette('top_movies') do
+          visit movies_path
+  
+          expect(current_path).to eq(movies_path)
+          expect(page).to have_content('Top Movies')
 
-  #       click_button('Find Top Rated Movies')
+          within(first('.movie')) do
+            expect(page).to have_css('.title')
+            expect(page).to have_css('.vote_avg')
+            page.find('.title').click
+            VCR.use_cassette('show_movie_details') do
+              save_and_open_page
+              expect(page).to have_button('Create Viewing Party for Movie')
+            end
+          end
+        end
+      end
 
-  #       expect(current_path).to eq(movies_path)
-  #       expect(page).to have_content('Top Movies')
-  #     end
-  #   end
+      describe 'I am shown search results even when' do 
+        it 'search terms include 2 words' do
+          VCR.use_cassette('two_keyword_search') do
+            visit discover_path
+
+            fill_in :keyword_search, with: 'toy story'
+            click_button('Find Movies')
+
+            expect(current_path).to eq(movies_path)
+            expect(page).to have_content('Search Results')
+          end
+        end
+
+        it 'search terms return 20 or less movie results' do
+          VCR.use_cassette('one_page_results') do
+            visit discover_path
+
+            fill_in :keyword_search, with: 'star wars'
+            click_button('Find Movies')
+
+            expect(current_path).to eq(movies_path)
+            expect(page).to have_content('Search Results')
+          end
+        end
+      end
+    end
   end
 
   describe 'As an unauthenticated user' do
