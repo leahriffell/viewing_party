be5cc3b69108a6724b2534ab5a5272dff1e0fdfc
Leah Riffell <leah.riffell@gmail.com>
2020-10-08 20:33:16 -0600

    Fix typo in test description

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Movies detail page' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
-    it 'I can see a movies title' do
+    it "I can see a movie's title" do
       VCR.use_cassette('show_movie_details') do
         visit(movie_show_path('524'))
         expect(page).to have_content('Casino')
