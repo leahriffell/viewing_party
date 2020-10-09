a2fc93fce3e82bcc3ddc429eee01f99adbfae6c3
Leah Riffell <leah.riffell@gmail.com>
2020-10-08 20:47:47 -0600

    Reorganize movies page tests

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -7,6 +7,42 @@ RSpec.describe 'Movies page' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
+    it "when I type movies_path in browser, I am defaulted to being shown top movies" do
+      VCR.use_cassette('top_movies') do
+
+        visit movies_path
+
+        expect(current_path).to eq(movies_path)
+        expect(page).to have_content('Top Movies')
+      end
+    end
+
+    describe 'I am shown search results even when' do 
+      it 'search terms include 2 words' do
+        VCR.use_cassette('two_keyword_search') do
+          visit discover_path
+
+          fill_in :keyword_search, with: 'toy story'
+          click_button('Find Movies')
+
+          expect(current_path).to eq(movies_path)
+          expect(page).to have_content('Search Results')
+        end
+      end
+
+      it 'search terms return 20 or less movie results' do
+        VCR.use_cassette('one_page_results') do
+          visit discover_path
+
+          fill_in :keyword_search, with: 'star wars'
+          click_button('Find Movies')
+
+          expect(current_path).to eq(movies_path)
+          expect(page).to have_content('Search Results')
+        end
+      end
+    end
+
   #   it 'when I click button to find top movies, I am redirected to movies page with top movies' do
   #     VCR.use_cassette('top_movies') do
   #       visit discover_path
