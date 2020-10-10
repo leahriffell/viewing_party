894d01bbff8583fbd2a50c7ed7e6d4ce878edcfb
Leah Riffell <leah.riffell@gmail.com>
2020-10-10 11:35:27 -0600

    Add new viewing party form tests

--- spec/features/viewing_party/new_spec.rb
+++ spec/features/viewing_party/new_spec.rb
@@ -2,8 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Create Viewing Party page' do
   describe 'As a authenticated user' do
+    it 'Can view but not edit movie title' do
+    end
 
-    # end
+    it 'Can see duration of party defaulted to runtime of movie' do 
+      #in minutes
+    end
+
+    it 'Can create viewing party (without editing duration)' do
+    end
+
+    it 'Can create viewing party (with edit of duration)' do
+    end
+ 
+    it 'Cannot create viewing party when no date is entered' do
+    end
+
+    it 'Cannot create viewing party when no time is entered' do
+
+    end
   end
 
   describe 'As an unauthenticated user' do
