364e19741b701f6330afc668e93d1cc3d4079a85
Leah Riffell <leah.riffell@gmail.com>
2020-10-11 15:26:21 -0600

    Add method for getting user's attendee status for party

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :party_users
+  has_many :parties, through: :party_users
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
 
@@ -9,5 +10,9 @@ class User < ApplicationRecord
 
   def any_parties?
     !party_users.empty?
+  end
+
+  def party_status(party_id)
+    self.party_users.where(party_id: party_id).pluck(:attendee_type).first
   end
 end
