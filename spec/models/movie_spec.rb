6f0ab7a2546fb50da81064bc13254a3548e0b8a9
Daniel Halverson <63027888+dhalverson@users.noreply.github.com>
2020-10-10 18:49:19 -0600

    Adds instance method tests for movies

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -15,16 +15,34 @@ describe Movie, type: :model do
       end
     end
 
-    it 'display genres' do
+    it 'can display genres' do
       VCR.use_cassette('show_movie_details') do
         genre_array = [{"id": 35,"name": "Comedy"}, {"id": 18,"name": "Drama"}]
         expect(@movie.display_genres(genre_array)).to eq(["Comedy", "Drama"])
       end
     end
 
-    it 'display runtime in hours and minutes' do
+    it 'can display runtime in hours and minutes' do
       VCR.use_cassette('show_movie_details') do
         expect(@movie.total_runtime(179)).to eq('2h 59m')
+      end
+    end
+
+    it 'can display the first 10 cast names and characters' do
+      VCR.use_cassette('show_movie_details') do
+
+      end
+    end
+
+    it 'can count the total number of reviews' do
+      VCR.use_cassette('show_movie_details') do
+
+      end
+    end
+
+    it 'can display the review author, url, and body' do
+      VCR.use_cassette('show_movie_details') do
+
       end
     end
   end
