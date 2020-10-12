356245240b97bb34d047d5165eaeab5a80dacff3
Leah Riffell <leah.riffell@gmail.com>
2020-10-11 21:23:47 -0600

    Add button to discover movies from dashboard

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Discover page' do
+RSpec.describe 'Dashboard page' do
   describe 'As a authenticated user' do
     before :each do
       @user = FactoryBot.create(:user)
@@ -8,15 +8,19 @@ RSpec.describe 'Discover page' do
     end
 
     it 'Can click a button to discover movies page' do
-      click_button('Discover Movies')
+      VCR.use_cassette('top_movies') do
+        visit dashboard_path
+        click_button('Discover Movies')
 
-      expect(current_path).to eq(movies_path)
+        expect(current_path).to eq(movies_path)
+      end
     end
+
   end
 
   describe 'As an unauthenticated user' do
     it 'When manually type in /dashboard, I see an error or warning' do
-      visit discover_path
+      visit dashboard_path
 
       expect(page).to have_content("The page you were looking for doesn't exist.")
     end
