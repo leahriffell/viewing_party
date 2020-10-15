d2689b867fbf019cfbc6a57899a3409f7a3ee45b
Leah Riffell <leah.riffell@gmail.com>
2020-10-14 18:04:01 -0600

    Remove unnecssary API key helper method

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -34,10 +34,6 @@ class MovieService
     'include_adult=false'
   end
 
-  def self.movies_api_key
-    ENV['MOVIES_API_KEY']
-  end
-
   def self.conn
     Faraday.new(url: 'https://api.themoviedb.org')
   end
@@ -48,22 +44,22 @@ class MovieService
 
   def self.top_movies_endpoint(page_num)
     sort_by = 'vote_average.desc'
-    conn.get("3/discover/movie?api_key=#{movies_api_key}&#{language('en-US')}&sort_by=#{sort_by}&#{exclude_adult}&page=#{page_num}&vote_count.gte=300")
+    conn.get("3/discover/movie?api_key=#{ENV['MOVIES_API_KEY']}&#{language('en-US')}&sort_by=#{sort_by}&#{exclude_adult}&page=#{page_num}&vote_count.gte=300")
   end
 
   def self.keyword_search_endpoint(page_num, keywords)
-    conn.get("3/search/movie?api_key=#{movies_api_key}&#{language('en-US')}&#{exclude_adult}&page=#{page_num}&query=#{keywords}")
+    conn.get("3/search/movie?api_key=#{ENV['MOVIES_API_KEY']}&#{language('en-US')}&#{exclude_adult}&page=#{page_num}&query=#{keywords}")
   end
 
   def self.movie_show_endpoint(id)
-    conn.get("3/movie/#{id}?api_key=#{movies_api_key}")
+    conn.get("3/movie/#{id}?api_key=#{ENV['MOVIES_API_KEY']}")
   end
 
   def self.movie_cast_endpoint(id)
-    conn.get("3/movie/#{id}/credits?api_key=#{movies_api_key}")
+    conn.get("3/movie/#{id}/credits?api_key=#{ENV['MOVIES_API_KEY']}")
   end
 
   def self.movie_review_endpoint(id)
-    conn.get("3/movie/#{id}/reviews?api_key=#{movies_api_key}&#{language('en-US')}")
+    conn.get("3/movie/#{id}/reviews?api_key=#{ENV['MOVIES_API_KEY']}&#{language('en-US')}")
   end
 end
